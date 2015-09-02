8cb464673daa3de0dc5aa7a736e9288c0f59e3ec
Julian Stewart <student@epicodus.com>
2015-09-02 16:45:43 -0700

    update routes, getters and setters; add compressed sql databases to repository.

--- app/app.php
+++ app/app.php
@@ -2,11 +2,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Contact.php";
 
-    session_start();
+    // session_start();
 
-    if (empty($_SESSION['list_of_contacts'])) {
-        $_SESSION['list_of_contacts'] = array();
-    }
+    // if (empty($_SESSION['list_of_contacts'])) {
+    //     $_SESSION['list_of_contacts'] = array();
+    // }
 
     $app = new Silex\Application();
 
--- src/Contact.php
+++ src/Contact.php
@@ -1,25 +1,27 @@
 <?php
 class Contact
 {
+    private $id;
     private $name;
     private $phone_number;
     private $address;
 
-    function __construct($name, $phone_number, $address)
+    function __construct($id, $name, $phone_number, $address)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->phone_number = $phone_number;
         $this->address = $address;
     }
 
-    function setName($new_name)
-    {
-        $this->name = (string) $new_name;
-    }
-
     function getName()
     {
         return $this->name;
+    }
+
+    function setName($new_name)
+    {
+        $this->name = (string) $new_name;
     }
 
     // function setPhoneNumber($new_phone_number)
@@ -42,10 +44,10 @@ class Contact
     //     return $this->address;
     // }
     //
-    // function save()
-    // {
-    //     array_push($_SESSION['list_of_contacts'], $this);
-    // }
+    function save()
+    {
+        array_push($_SESSION['list_of_contacts'], $this);
+    }
     //
     // static function getAll()
     // {
