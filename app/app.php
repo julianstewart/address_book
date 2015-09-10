7350fc6e92ec381d658cd14fd4168cebf1011325
Julian Stewart <student@epicodus.com>
2015-09-10 15:21:07 -0700

    update routes

--- app/app.php
+++ app/app.php
@@ -4,18 +4,27 @@
 
     $app = new Silex\Application();
 
+    $server = 'mysql:host=localhost;dbname=address_book';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    })
+
+    $app->get("/contacts", function() use ($app) {
 
         return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
 
     });
 
     $app->post("/contacts", function() use ($app) {
-        $contact = new Contact($_POST['contact_name']);
+        $contact = new Contact($_POST['contact_name', 'contact_phone_number', 'contact_address']);
         $contact->save();
         return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
     });
