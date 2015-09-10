bb7df2b75746ecf18c8a0ae6bf8d986994907039
Julian Stewart <student@epicodus.com>
2015-09-10 12:43:11 -0700

    add find test script

--- tests/ContactTest.php
+++ tests/ContactTest.php
@@ -25,16 +25,16 @@
             $name = "Jane Doe";
             $phone_number = "";
             $address = "";
-            $test_Contact = new Contact($name, $phone_number, $address);
+            $test_contact = new Contact($name, $phone_number, $address);
 
             //act
-            $result = $test_Contact->getName();
+            $result = $test_contact->getName();
 
             //assert
             $this->assertEquals($name, $result);
 
             //for debugging
-            // var_dump($test_Contact);
+            // var_dump($test_contact);
         }
 
         function test_getPhoneNumber()
@@ -43,16 +43,16 @@
             $name = "";
             $phone_number = "555-555-5555";
             $address = "";
-            $test_Contact = new Contact($name, $phone_number, $address);
+            $test_contact = new Contact($name, $phone_number, $address);
 
             //act
-            $result = $test_Contact->getPhoneNumber();
+            $result = $test_contact->getPhoneNumber();
 
             //assert
             $this->assertEquals($phone_number, $result);
 
             //for debugging
-            // var_dump($test_Contact);
+            // var_dump($test_contact);
         }
 
         function test_getAddress()
@@ -61,16 +61,16 @@
             $name = "";
             $phone_number = "";
             $address = "5 Main Street, Anytown, Anystate 55555";
-            $test_Contact = new Contact($name, $phone_number, $address);
+            $test_contact = new Contact($name, $phone_number, $address);
 
             //act
-            $result = $test_Contact->getAddress();
+            $result = $test_contact->getAddress();
 
             //assert
             $this->assertEquals($address, $result);
 
             //for debugging
-            // var_dump($test_Contact);
+            // var_dump($test_contact);
         }
 
         function test_save()
@@ -79,17 +79,17 @@
             $name = "Jane Doe";
             $phone_number = "555-555-5555";
             $address = "5 Main Street, Anytown, Anystate 55555";
-            $test_Contact = new Contact($name, $phone_number, $address);
+            $test_contact = new Contact($name, $phone_number, $address);
 
             //act
-            $test_Contact->save();
+            $test_contact->save();
 
             //assert
             $result = Contact::getAll();
-            $this->assertEquals($test_Contact, $result[0]);
+            $this->assertEquals($test_contact, $result[0]);
 
             //for debugging
-            // var_dump($test_Contact);
+            // var_dump($test_contact);
         }
 
         function test_getAll()
@@ -101,20 +101,20 @@
             $phone_number2 = "666-666-6666";
             $address = "5 Main Street, Anytown, Anystate 55555";
             $address2 = "6 Main Street, Anytown, Anystate 66666";
-            $test_Contact = new Contact($name, $phone_number, $address);
-            $test_Contact->save();
-            $test_Contact2 = new Contact($name2, $phone_number2, $address2);
-            $test_Contact2->save();
+            $test_contact = new Contact($name, $phone_number, $address);
+            $test_contact->save();
+            $test_contact2 = new Contact($name2, $phone_number2, $address2);
+            $test_contact2->save();
 
             //act
             $result = Contact::getAll();
 
             //assert
-            $this->assertEquals([$test_Contact, $test_Contact2], $result);
+            $this->assertEquals([$test_contact, $test_contact2], $result);
 
             //for debugging
-            // var_dump($test_Contact);
-            // var_dump($test_Contact2);
+            // var_dump($test_contact);
+            // var_dump($test_contact2);
         }
 
         function test_getId()
@@ -124,16 +124,42 @@
             $phone_number = "555-555-5555";
             $address = "5 Main Street, Anytown, Anystate 55555";
             $id = 1;
-            $test_Contact = new Contact($name, $phone_number, $address, $id);
+            $test_contact = new Contact($name, $phone_number, $address, $id);
 
             //act
-            $result = $test_Contact->getId();
+            $result = $test_contact->getId();
 
             //assert
             $this->assertEquals(1, $result);
 
             //for debugging
-            var_dump($test_Contact);
+            // var_dump($test_contact);
+        }
+
+        function test_find()
+        {
+            //arrange
+            $name = "Jane Doe";
+            $name2 = "John Doe";
+            $phone_number = "555-555-5555";
+            $phone_number2 = "666-666-6666";
+            $address = "5 Main Street, Anytown, Anystate 55555";
+            $address2 = "6 Main Street, Anytown, Anystate 66666";
+            $test_contact = new Contact($name, $phone_number, $address);
+            $test_contact->save();
+            $test_contact2 = new Contact($name2, $phone_number2, $address2);
+            $test_contact2->save();
+
+            //act
+            $id = $test_contact->getId();
+            $result = Contact::find($id);
+
+            //assert
+            $this->assertEquals($test_contact, $result);
+
+            //for debugging
+            var_dump($test_contact);
+            var_dump($test_contact2);
         }
     }
 
