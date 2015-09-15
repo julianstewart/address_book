e7c2d464e20c5b9ee97b22e8ee5a8b8dabc9811f
Julian Stewart <student@epicodus.com>
2015-09-15 12:21:40 -0700

    pass Category find test

--- src/Category.php
+++ src/Category.php
@@ -50,6 +50,18 @@
             $GLOBALS['DB']->exec("DELETE FROM categories;");
         }
 
+        static function find($search_id)
+        {
+            $found_category = null;
+            $categories = Category::getAll();
+            foreach($categories as $category) {
+                $category_id = $category->getId();
+                if ($category_id == $search_id) {
+                    $found_category = $category;
+                }
+            }
+            return $found_category;
+        }
     }
 
 ?>
