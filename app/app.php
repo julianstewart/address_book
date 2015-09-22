925a895f464a8d83830cecb0cf9a0c780143803f
Julian Stewart <student@epicodus.com>
2015-09-22 12:55:46 -0700

    update app routes

--- app/app.php
+++ app/app.php
@@ -33,7 +33,7 @@
 
     $app->get("/categories/{id}", function($id) use ($app) {
         $category = Category::find($id);
-        return $app['twig']->render('category.html.twig', array('category' => $category, 'contact'->getTasks()));
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'contact' => $category->getContacts()));
     });
 
     $app->patch("/contacts/{id}", function($id) use ($app) {
